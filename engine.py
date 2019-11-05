5a5603b47951491e5a53b429df836d2d9dde4342
Asylumrunner <asylumrunner@gmail.com>
2019-11-04 19:17:26 -0600

    Rip initialization code into separate function

--- engine.py
+++ engine.py
@@ -9,114 +9,119 @@ from game_states import GameStates
 from components.fighter import Fighter
 from game_messages import MessageLog
 import game_constants
-    
-def initialize_player(message_log):
-    return Entity(int(game_constants.screen_width/2), int(game_constants.screen_height/2), '@',
-     libtcod.white, "Player", True, RenderOrder.ACTOR, Fighter(hp=30, defense=2, power=5), None, message_log)
 
-def main():
+class Engine():
+    def __init__(self):
+        # Set up the game window
+        libtcod.console_set_custom_font('spritesheet.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+        libtcod.console_init_root(game_constants.screen_width, game_constants.screen_height, 'Ascetic of the Cabal', False, libtcod.RENDERER_SDL2, vsync=True)
 
-    # Set up the game window
-    libtcod.console_set_custom_font('spritesheet.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
-    libtcod.console_init_root(game_constants.screen_width, game_constants.screen_height, 'Ascetic of the Cabal', False, libtcod.RENDERER_SDL2, vsync=True)
+        # Establish the primary console as well as the detail panel
+        self.con = libtcod.console.Console(game_constants.screen_width, game_constants.screen_height)
+        self.panel = libtcod.console.Console(game_constants.screen_width, game_constants.panel_height)
 
-    # Establish the primary console as well as the detail panel
-    con = libtcod.console.Console(game_constants.screen_width, game_constants.screen_height)
-    panel = libtcod.console.Console(game_constants.screen_width, game_constants.panel_height)
+        # Create and initialize the Message Log
+        self.message_log = MessageLog()
 
-    # Create and initialize the Message Log
-    message_log = MessageLog()
+        # Create a game map and fill it with enemies
+        self.game_map = GameMap(self.message_log)
+        self.player = self.initialize_player()
+        self.entities = [self.player]
+        self.game_map.make_map(self.player, self.entities)
 
-    # Create a game map and fill it with enemies
-    game_map = GameMap(message_log)
-    player = initialize_player(message_log)
-    entities = [player]
-    game_map.make_map(player, entities)
+        # Initialize the FOV
+        self.fov_recompute = True
+        self.fov_map = initialize_fov(self.game_map)
 
-    # Initialize the FOV
-    fov_recompute = True
-    fov_map = initialize_fov(game_map)
+        # Create references for the player input
+        self.key = libtcod.Key()
+        self.mouse = libtcod.Mouse()
 
-    # Create references for the player input
-    key = libtcod.Key()
-    mouse = libtcod.Mouse()
-
-    # Establish the Game State
-    game_state = GameStates.PLAYERS_TURN
-    game_running = True
-
-    # Game Loop
-    while game_running:
-        # Check input streams for an event
-        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
-
-        # If we need to recompute fov, do so
-        if fov_recompute:
-            recompute_fov(fov_map, player.x, player.y)
+        # Establish the Game State
+        self.game_state = GameStates.PLAYERS_TURN
+        self.game_running = True
         
-        # Render the game world according to current FOV, mark FOV recompute as complete, and flush to console
-        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, mouse)
-        fov_recompute = False
-        libtcod.console_flush()
+    def initialize_player(self):
+        return Entity(int(game_constants.screen_width/2), int(game_constants.screen_height/2), '@',
+        libtcod.white, "Player", True, RenderOrder.ACTOR, Fighter(hp=30, defense=2, power=5), None, self.message_log)
 
-        # Populate the game map with characters
-        clear_all(con, entities)
-
-        # Interpret the input into a game action
-        action = handle_keys(key)
-        move = action.get('move')
-        exit = action.get('exit')
-        fullscreen = action.get('fullscreen')
-
-        player_turn_results = []
-
-        # If players turned and it's their turn to move
-        if move and game_state == GameStates.PLAYERS_TURN:
-            # Calculate where they should move
-            dx, dy = move
-            destination_x = player.x + dx
-            destination_y = player.y + dy
-
-            if not game_map.is_blocked(destination_x, destination_y):
-                # If they're not about to walk into a wall, check for enemies at the destination
-                target = get_blocking_entities_at_location(entities, destination_x, destination_y)
-                if target:
-                    # If there are enemies, attack them
-                    player.fighter.attack(target)
-                else:
-                    # If there are not enemies, move and mark FOV for recomputation
-                    player.move(dx, dy)
-                    fov_recompute = True
-                game_state = GameStates.ENEMY_TURN
-            
-        # Exit the game
-        if exit:
-            return True
-        
-        # Set the game to fullscreen
-        if fullscreen:
-            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
-
-        dead_entities = [entity for entity in entities if not entity.fighter.isAlive() and entity.char != '%']
+    def cull_dead(self):
+        # Finds every entity in the game world with 0 health and kills them
+        # Returns true if it kills the player, otherwise false
+        player_killed = False
+        dead_entities = [entity for entity in self.entities if not entity.fighter.isAlive() and entity.char != '%']
 
         if dead_entities:
             for dead_entity in dead_entities:
                 dead_entity.fighter.die()
-                if dead_entity == player:
-                    game_state = GameStates.PLAYER_DEAD
+                if dead_entity == self.player:
+                    player_killed = True
+        return player_killed
 
-        if game_state == GameStates.ENEMY_TURN:
-            for entity in entities:
-                if entity.ai:
-                    entity.ai.take_turn(player, fov_map, game_map, entities)
-                    dead_entities = [entity for entity in entities if not entity.fighter.isAlive() and entity.char != '%']
-                    if dead_entities:
-                        for dead_entity in dead_entities:
-                            dead_entity.fighter.die()
-                            if dead_entity == player:
-                                game_state = GameStates.PLAYER_DEAD
-            if game_state != GameStates.PLAYER_DEAD:
-                game_state = GameStates.PLAYERS_TURN
+    def main(self):
+        # Game Loop
+        while self.game_running:
+            # Check input streams for an event
+            libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, self.key, self.mouse)
+
+            # If we need to recompute fov, do so
+            if self.fov_recompute:
+                recompute_fov(self.fov_map, self.player.x, self.player.y)
+            
+            # Render the game world according to current FOV, mark FOV recompute as complete, and flush to console
+            render_all(self.con, self.panel, self.entities, self.player, self.game_map, self.fov_map, self.fov_recompute, self.message_log, self.mouse)
+            self.fov_recompute = False
+            libtcod.console_flush()
+
+            # Populate the game map with characters
+            clear_all(self.con, self.entities)
+
+            # Interpret the input into a game action
+            action = handle_keys(self.key)
+            move = action.get('move')
+            exit = action.get('exit')
+            fullscreen = action.get('fullscreen')
+
+            # If players turned and it's their turn to move
+            if move and self.game_state == GameStates.PLAYERS_TURN:
+                # Calculate where they should move
+                dx, dy = move
+                destination_x = self.player.x + dx
+                destination_y = self.player.y + dy
+
+                if not self.game_map.is_blocked(destination_x, destination_y):
+                    # If they're not about to walk into a wall, check for enemies at the destination
+                    target = get_blocking_entities_at_location(self.entities, destination_x, destination_y)
+                    if target:
+                        # If there are enemies, attack them
+                        self.player.fighter.attack(target)
+                    else:
+                        # If there are not enemies, move and mark FOV for recomputation
+                        self.player.move(dx, dy)
+                        self.fov_recompute = True
+                    self.game_state = GameStates.ENEMY_TURN
+                
+            # Exit the game
+            if exit:
+                return True
+            
+            # Set the game to fullscreen
+            if fullscreen:
+                libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+            if self.cull_dead():
+                self.game_state = GameStates.PLAYER_DEAD
+
+            if self.game_state == GameStates.ENEMY_TURN:
+                for entity in self.entities:
+                    if entity.ai:
+                        entity.ai.take_turn(self.player, self.fov_map, self.game_map, self.entities)
+                        if self.cull_dead():
+                            self.game_state = GameStates.PLAYER_DEAD
+
+                if self.game_state != GameStates.PLAYER_DEAD:
+                    self.game_state = GameStates.PLAYERS_TURN
 
 if __name__ == '__main__':
-    main()
+    engine = Engine()
+    engine.main()
