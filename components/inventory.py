7d9194300bb638a71fc0f7d6593a9a7d06a9f9f6
Asylumrunner <asylumrunner@gmail.com>
2020-02-26 19:07:55 -0600

    Add framework for equippables

--- components/inventory.py
+++ components/inventory.py
@@ -1,10 +1,19 @@
 import tcod as libtcod
+from item import ItemType
 from game_messages import Message
 
 class Inventory:
     def __init__(self, capacity):
         self.capacity = capacity
         self.items = []
+        self.equipped = {
+            'HEAD': None,
+            'BODY': None,
+            'LEGS': None,
+            'TRINKET': None,
+            'MELEE': None,
+            'RANGED': None
+        }
 
     def add_item(self, item):
         if len(self.items) >= self.capacity:
@@ -19,3 +28,32 @@ class Inventory:
             self.owner.log.add_message(Message("{0} broke!".format(item.name), libtcod.red))
         except:
             print("Item not found")
+    
+    def equip_item(self, item):
+        item_type = item.item_type
+        something_equipped = True
+        if item_type == ItemType.HEAD:
+            last_item = self.equipped['HEAD']
+            self.equipped['HEAD'] = item
+        elif if item_type == ItemType.BODY:
+            last_item = self.equipped['BODY']
+            self.equipped['BODY'] = item
+        elif if item_type == ItemType.LEGS:
+            last_item = self.equipped['LEGS']
+            self.equipped['LEGS'] = item
+        elif if item_type == ItemType.TRINKET:
+            last_item = self.equipped['TRINKET']
+            self.equipped['TRINKET'] = item
+        elif if item_type == ItemType.MELEE:
+            last_item = self.equipped['MELEE']
+            self.equipped['MELEE'] = item
+        elif if item_type == ItemType.RANGED:
+            last_item = self.equipped['RANGED']
+            self.equipped['RANGED'] = item
+        else:
+            something_equipped = False
+        
+        if(something_equipped):
+            self.owner.log.add_message(Message("Equipped {0}".format(item.name), libtcod.green))
+            if(last_item):
+                self.owner.log.add_message(Message("Unequipped {0}".format(last_item.name), libtcod.green))
--- components/item.py
+++ components/item.py
@@ -1,8 +1,20 @@
+from enum import Enum
+
+class ItemType(Enum):
+    NONE = 0
+    HEAD = 1
+    BODY = 2
+    LEGS = 3
+    TRINKET = 4
+    MELEE = 5
+    RANGED = 6
+
 class Item:
-    def __init__(self, use_function=None, uses=-99, **kwargs):
+    def __init__(self, use_function=None, uses=-99, item_type=ItemType.NONE, **kwargs):
         self.use_function = use_function
         self.uses = uses
         self.function_kwargs = kwargs
+        self.item_type = item_type
     
     def use(self, *args):
         for function in self.use_function:
