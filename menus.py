6d8f22fa14f5feaa628cd3dbc43ca317dde210d8
Asylumrunner <asylumrunner@gmail.com>
2021-04-10 13:56:42 -0700

    Finish the Great Encommenting

--- menus.py
+++ menus.py
@@ -2,19 +2,25 @@ import tcod as libtcod
 import game_constants
 from inspect_functions import inspect_entity
 
+#A set of functions designed to assist with drawing various menus (inventory, dialogue, etc)
+
 def menu(con, header, options, width, itemized=True):
+    #hard-wiring in a maximimal list length of 26 items
     if len(options) > 26:
         raise ValueError("Too many menu options: " + len(options))
 
+    # These functions together generate the number of lines of screen space that will be needed 
+    #  to print the menu
     header_height = libtcod.console_get_height_rect(con, 0, 0, width, game_constants.screen_height, header)
     body_height = libtcod.console_get_height_rect(con, 0, 0, width, game_constants.screen_height, "\n".join(options))
     height = body_height + header_height * 2
 
+    # Then, a window of the needed proportions is drawn, its foreground drawn and a window printed
     window = libtcod.console.Console(width, height)
-
     libtcod.console_set_default_foreground(window, libtcod.white)
     libtcod.console_print_rect_ex(window, 0, 0, width, height, libtcod.BKGND_NONE, libtcod.LEFT, header)
 
+    # Then, the header is drawn and menu options are drawn with letter indexes added to them (if itemized is true)
     y = header_height
     letter_index = ord('a')
     for option_text in options:
@@ -23,13 +29,16 @@ def menu(con, header, options, width, itemized=True):
         y += offset
         letter_index += 1
     
+    # Then you blit the window to console
     libtcod.console_blit(window, 0, 0, width, height, 0, game_constants.screen_width-game_constants.character_portrait_width-width, game_constants.screen_height-height, 1.0, 0.7)
 
+# Draws an image to a window. Used for character portraits in dialogue
 def draw_picture(con, image, width):
     picture = libtcod.console.Console(game_constants.portrait_width, game_constants.portrait_height)
     libtcod.image_blit_rect(image, picture, 0, 0, -1, -1, libtcod.BKGND_NONE)
     libtcod.console_blit(picture, 0, 0, game_constants.portrait_width, game_constants.portrait_height, con, x, y, 1.0, 0.7)
 
+# Creates a menu for the player's inventory
 def inventory_menu(con, header, inventory):
     if len(inventory.items) == 0:
         options = ['Inventory empty']
@@ -38,20 +47,24 @@ def inventory_menu(con, header, inventory):
     
     menu(con, header, options, 44)
 
+# Creates a dialogue menu for a given dialogue target
 def dialogue_menu(con, dialogue_target):
     dialogue_target_name = dialogue_target.name
     conversation_state = dialogue_target.get_component("Character").get_conversation()
     menu(con, "<" + dialogue_target_name + ">" + "\n" + conversation_state.utterance, conversation_state.choices, 44)
 
+# Creates a menu for the currently equipped gear of the player
 def equipped_menu(con, inventory):
     listed_options = [key + ": " + (inventory.equipped[key].name if inventory.equipped[key] else "None") for key in inventory.equipped]
     menu(con, "EQUIPPED GEAR", listed_options, 44)
 
+# Creates a menu for when the player chooses to inspect an NPC or item
 def inspect_menu(con, inspect_target):
     inspect_details = inspect_entity(inspect_target)
     menu(con, "INSPECTED ENTITY: {}".format(inspect_target.name), inspect_details, 44, False)
     #add a call to draw_picture here
 
+# Draws a menu for item descriptions
 def item_detail_menu(con, item):
     item_type = item.item.item_type.name
     description = game_constants.items[item.name]['description']
@@ -59,6 +72,7 @@ def item_detail_menu(con, item):
     listed_attributes = [item_type, description, uses]
     menu(con, item.name, listed_attributes, 44, False)
 
+# Draws the main menu of the game on boot
 def main_menu(con, background_image):
     libtcod.image_blit_2x(background_image, con, 0, 0)
 
--- render_functions.py
+++ render_functions.py
@@ -11,6 +11,7 @@ class RenderOrder(Enum):
     ITEM = 2
     ACTOR = 3
 
+# returns a list of capitalized names of anything under the mouse's current position
 def get_names_under_mouse(mouse, entities, fov_map):
     (x, y) = (mouse.cx, mouse.cy)
 
@@ -19,6 +20,7 @@ def get_names_under_mouse(mouse, entities, fov_map):
 
     return names.capitalize()
 
+# Renders a bar (like a health or mana bar) that is proportionately filled based on some value versus a maximum
 def render_bar(panel, x, y, total_width, name, value, maximum, bar_color, back_color):
     bar_width = int(float(value) / maximum * total_width)
 
@@ -33,15 +35,20 @@ def render_bar(panel, x, y, total_width, name, value, maximum, bar_color, back_c
     libtcod.console_print_ex(panel, int(x + total_width / 2), y, libtcod.BKGND_NONE, libtcod.CENTER, '{0}: {1}/{2}'.format(name, value, maximum))
 
     
-
+# Renders everything onto the game screen. Probably the single most complicated function in the game atm
 def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, mouse, game_state, target):
+
+    # The good news is, we can reuse and skip recalculations for a lot of shit if the fov doesn't need to be recalculated
     if fov_recompute:
+        # Collect color info for the various tiles based on the floor
         colors = game_map.get_floor_info()['colors']
         for y in range(game_map.height):
             for x in range(game_map.width):
+                #iterating through every tile in the map, we check to see if it's visible, and if it's an empty space or a wall
                 visible = fov_map.fov[y][x]
                 wall = game_map.tiles[x][y].block_sight
 
+                #if it's visible, we draw either a wall or the ground, based on which it is
                 if visible:
                     if wall:
                         libtcod.console_set_default_foreground(con, colors['light_wall'])
@@ -51,25 +58,33 @@ def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, m
                         libtcod.console_put_char(con, x, y, libtcod.CHAR_BLOCK1, libtcod.BKGND_NONE)
                     game_map.tiles[x][y].explored = True
                 elif game_map.tiles[x][y].explored:
+                    #if it is not visible, but the player has previously been there, we draw a darker version of the sprite instead
                     if wall:
                         libtcod.console_set_default_foreground(con, colors['dark_wall'])
                         libtcod.console_put_char(con, x, y, libtcod.CHAR_BLOCK3, libtcod.BKGND_NONE)
                     else:
                         libtcod.console_set_default_foreground(con, colors['dark_ground'])
                         libtcod.console_put_char(con, x, y, libtcod.CHAR_BLOCK1, libtcod.BKGND_NONE)
+    
+    #We then sort every entity by render order. Entities later in the render order will appear on top of things earlier
     entities_in_render_order = sorted(entities, key= lambda x: x.render_order.value)
+
+    #then, we draw every entity on the map
     for entity in entities_in_render_order:
         draw_entity(con, entity, game_map, fov_map)
 
+    # Now, we wipe the panel (that is, the bar beneath the main view)
     libtcod.console_set_default_background(panel, libtcod.black)
     libtcod.console_clear(panel)
 
+    # We then print every message in the message log to the panel
     y = 1
     for message in message_log.messages:
         libtcod.console_set_default_foreground(panel, message.color)
         libtcod.console_print_ex(panel, message_log.x, y, libtcod.BKGND_NONE, libtcod.LEFT, message.text)
         y += 1
 
+    # This goddamn mess renders the constant UI elements, currently bound to the bottom left of the screen
     render_bar(panel, 1, 1, game_constants.bar_width, 'HP', player.get_component("Fighter").hp, player.get_component("Fighter").max_hp, libtcod.light_red, libtcod.darker_red)
     libtcod.console_set_default_foreground(panel, libtcod.green)
     libtcod.console_print_ex(panel, 1, 3, libtcod.BKGND_NONE, libtcod.LEFT, "${}".format(player.get_component("Fighter").money))
@@ -80,12 +95,15 @@ def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, m
     #libtcod.console_print_ex(panel, 1, 6, libtcod.BKGND_NONE, libtcod.LEFT, "({}, {})".format(mouse.cx, mouse.cy))
     libtcod.console_print_ex(panel, 1, 5, libtcod.BKGND_NONE, libtcod.LEFT,' '.join(player.get_component("StatusContainer").get_statuses()))
 
+    # This bit prints out a list of everything currently under the mouse
     libtcod.console_set_default_foreground(panel, libtcod.light_grey)
     libtcod.console_print_ex(panel, 1, 0, libtcod.BKGND_NONE, libtcod.LEFT, get_names_under_mouse(mouse, entities, fov_map))
     
+    # Now that the main console and the panel are done, blit them onto the screen
     libtcod.console_blit(con, 0, 0, game_constants.screen_width, game_constants.screen_height, 0, 0, 0)
     libtcod.console_blit(panel, 0, 0, game_constants.screen_width, game_constants.panel_height, 0, 0, game_constants.panel_y)
 
+    # Depending on various game states, draw a menu onto the screen
     if game_state == GameStates.INVENTORY_OPEN:
         inventory_menu(con, "Press the indicated key to use item", player.get_component("Inventory"))
     elif game_state == GameStates.DIALOGUE and target:
@@ -96,19 +114,23 @@ def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, m
     elif game_state == GameStates.INSPECT_OPEN:
         inspect_menu(con, target)
 
+# Removes every entity in the game from the screen
 def clear_all(con, entities):
     for entity in entities:
         clear_entity(con, entity)
 
+# Draws a character portrait. This... needs some work
 def character_portrait(target):
     portrait_image = libtcod.image_load('green.png')
     libtcod.image_blit_rect(portrait_image, 0, game_constants.screen_width-game_constants.character_portrait_width, game_constants.panel_y, game_constants.character_portrait_width, game_constants.panel_height, libtcod.BKGND_SET)
 
+# Draws an entity onto the screen, using the entity information to determine icon and color
 def draw_entity(con, entity, game_map, fov_map):
     if fov_map.fov[entity.y][entity.x] or (entity.has_component("Stairs") and game_map.tiles[entity.x][entity.y].explored):
         libtcod.console_set_default_foreground(con, entity.color)
         libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
 
+# CLears an individual entity from the console
 def clear_entity(con, entity):
     libtcod.console_put_char(con, entity.x, entity.y, ' ', libtcod.BKGND_NONE)
 
